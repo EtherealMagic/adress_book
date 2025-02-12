be81a2ee1c18ed66ead434ccd8fece18969d9861
Tobias Ivan Godoy <tobiasig2001@gmail.com>
2025-02-11 22:16:29 -0300

    arreglo de sintaxis.

--- ejercicio_agenda.py
+++ ejercicio_agenda.py
@@ -15,7 +15,7 @@ Bienvenid@ a su agenda.
     interfaz = input("ingrese una entrada: ")
     try:
         if interfaz == "1":
-            nombre = input("nombre del nuevo usuario: ").capitalize()
+            nombre = input("nombre del nuevo contacto: ").capitalize()
             telefono = input("número de telefono: ")
             mail = input("mail del nuevo contacto: ")
             try:
