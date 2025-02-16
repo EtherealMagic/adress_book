69cff358d31af1e414679291248a06dd52cac634
Tobias Ivan Godoy <tobiasig2001@gmail.com>
2025-02-16 13:14:52 -0300

    Update adress_book_exercise.py
    added coments to the code

--- adress_book_exercise.py
+++ adress_book_exercise.py
@@ -1,5 +1,5 @@
-contact_list = {}
-name_list = []
+contact_list = {}  # Dictionary to store contacts with name as the key and a list [phone, email] as the value
+name_list = []  # List to keep track of contact names
 
 while True:
 
@@ -14,14 +14,14 @@ Welcome to your address book.
 """)
     interface = input("Enter an option: ")
     try:
-        if interface == "1": 
+        if interface == "1":  # Adding a contact
             name = input("Name of the new contact: ").capitalize()
             phone = input("Phone number: ")
             email = input("Email of the new contact: ")
             try:
-                int(phone)
-                contact_list[name] = [phone, email]
-                name_list.append(name)
+                int(phone)  # Checking if the phone number is a valid integer
+                contact_list[name] = [phone, email]  # Storing the contact in the dictionary
+                name_list.append(name)  # Adding the name to the name list
                 input("""
 Contact saved successfully. Press enter to return.
 """)
@@ -32,16 +32,16 @@ Contact saved successfully. Press enter to return.
 Press enter to return.
 """)
 
-        elif interface == "2":
+        elif interface == "2":  # Searching for a contact
             entry = input("Name of the contact: ").capitalize()
-            contact = contact_list.get(entry)
-            if contact_list.get(entry) == None:
+            contact = contact_list.get(entry)  # Getting the contact details from the dictionary
+            if contact is None:  # If contact doesn't exist
                 print("Contact not found.")
                 input("""
 Press enter to return.
 """)
 
-            else: 
+            else:  # Displaying the contact details if found
                 print(f"""
             Name: {entry}
             Number: {contact[0]}
@@ -52,11 +52,11 @@ Press enter to return.
 Press enter to return.
 """)
 
-        elif interface == "3":
+        elif interface == "3":  # Deleting a contact
             deleted = input("Contact to delete: ").capitalize()
-            if deleted in contact_list:
-                contact_list.pop(deleted)
-                name_list.remove(deleted)
+            if deleted in contact_list:  # Checking if the contact exists
+                contact_list.pop(deleted)  # Removing the contact from the dictionary
+                name_list.remove(deleted)  # Removing the name from the name list
                 print("Contact successfully deleted.")
             else:
                 print("User not found.")
@@ -64,8 +64,8 @@ Press enter to return.
 Press enter to return.
 """)
 
-        elif interface == "4":
-            if not name_list:
+        elif interface == "4":  # Viewing all contacts
+            if not name_list:  # If there are no contacts in the list
                 input("""
 You haven't saved any contacts yet. Press enter to return.
 """)
@@ -73,15 +73,15 @@ You haven't saved any contacts yet. Press enter to return.
                 print(f"""The following people are in your contact list: 
                     """)
                 for name in name_list:
-                    print(f"- {name}")
+                    print(f"- {name}")  # Displaying all saved contact names
                 input("""
 Press enter to return.
 """)
 
-        elif interface == "5":
+        elif interface == "5":  # Modifying a contact
             entry = input("Contact to modify: ").capitalize()
         
-            if entry in contact_list:
+            if entry in contact_list:  # Checking if the contact exists
                 try:
                     modification = input("""
 1 - Modify name.
@@ -89,33 +89,32 @@ Press enter to return.
 3 - Modify email.
 Enter an option: """)
 
-                    if modification == "1":
+                    if modification == "1":  # Modifying the contact's name
                         new_name = input("Enter new name: ").capitalize()
 
-                        if new_name not in name_list:
-                            contact_list[new_name] = contact_list.pop(entry)
-                            
-                            name_list.append(new_name)
-                            name_list.remove(entry)
+                        if new_name not in name_list:  # Checking if the new name already exists
+                            contact_list[new_name] = contact_list.pop(entry)  # Renaming the contact
+                            name_list.append(new_name)  # Adding the new name to the name list
+                            name_list.remove(entry)  # Removing the old name from the list
 
                             input("\nContact successfully modified. Press enter to return.")
                         else:
                             input("\nA contact with that name already exists. Press enter to return.")
 
-                    elif modification == "2":
+                    elif modification == "2":  # Modifying the phone number
                         new_number = input("Enter new number: ")
                         try:
-                            int(new_number)  
-                            contact_list[entry][0] = new_number  
+                            int(new_number)  # Checking if the new number is valid
+                            contact_list[entry][0] = new_number  # Updating the phone number
 
                             input("\nNumber successfully modified. Press enter to return.")
                             
                         except ValueError:
                             input("\nInvalid number. Press enter to return.")
 
-                    elif modification == "3":
+                    elif modification == "3":  # Modifying the email
                         new_email = input("Enter new email: ")
-                        contact_list[entry][1] = new_email  
+                        contact_list[entry][1] = new_email  # Updating the email
 
                         input("\nEmail successfully modified. Press enter to return.")
 
@@ -128,9 +127,10 @@ Enter an option: """)
             else:
                 input(f"\nThe contact '{entry}' is not in your contact list. Press enter to return.")
 
-        elif interface == "6":
+        elif interface == "6":  # Closing the program
             break
     
     except:
-        print("Invalid input.")
+        print("Invalid input.")  # Error handling for invalid options
         input("Please try again.")
+
